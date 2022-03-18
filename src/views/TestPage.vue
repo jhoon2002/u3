9d4b427b9f0337908cf59f4d43f07223500b1ba4
jhoon <jhoon2002@naver.com>
2022-03-18 17:51:42 +0900

    office

--- src/views/TestPage.vue
+++ src/views/TestPage.vue
@@ -5,25 +5,27 @@ import http from '@/api/http.js'
 const keyword = ref(null)
 const keywordRef = ref(null)
 const options = ref([])
+let timeout = null
 
 onMounted(() => {
     // ref 로 native element 를 가져와서 input 이벤트가 발생할때 model 에 연결된 변수에 값을 저장한다.
     const el = keywordRef.value.getNativeElement()
-    el.addEventListener('input', e => {
+    el.addEventListener('input', async e => {
         //model
         keyword.value = e.target.value
 
         //getData
-        setTimeout(async () => {
+        clearTimeout(timeout)
+        timeout = setTimeout(async () => {
             try {
                 const {
                     data: { users },
                 } = await http.get('/api/users/userid-name/' + e.target.value)
                 options.value = users.map(item => item.name + ' ' + item.birthday)
-            } catch {
-                //
+            } catch (e) {
+                console.log(e)
             }
-        }, 1000)
+        }, 400)
     })
 })
 
